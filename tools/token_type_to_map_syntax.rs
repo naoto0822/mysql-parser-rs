fb39fd0c8ee0406f65c2a61985312e89884d5861
naoto0822 <n.h.in.m.h@gmail.com>
2022-09-17 11:02:28 +0900

    added comment

--- tools/token_type_to_map_syntax.rs
+++ tools/token_type_to_map_syntax.rs
@@ -1,6 +1,7 @@
 use mysql_parser_rs::token_type::{str_for_tool_map_syntax, TokenType};
 use strum::IntoEnumIterator;
 
+/// token_type_to_map_syntax is generating map synax.
 fn main() {
     for tt in TokenType::iter() {
         let to_str = str_for_tool_map_syntax(&tt);
